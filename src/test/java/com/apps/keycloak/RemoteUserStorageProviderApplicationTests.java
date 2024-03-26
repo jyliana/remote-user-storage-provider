3d73a40ddb358ed50bd3530e0eaaa8d4c2fa8fcc
Inna Lapina <jyliana@gmail.com>
2024-03-26 15:44:27 +0200

    initial files

---
+++ src/test/java/com/apps/keycloak/RemoteUserStorageProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteUserStorageProviderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
